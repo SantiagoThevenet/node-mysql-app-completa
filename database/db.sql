fa88ce5e27db09dfe76ec9e4e165d4720b874ed3
santiago <santithevenetvalles@gmail.com>
2023-02-08 13:50:28 +0100

    Creada base de datos

---
+++ database/db.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS database_links;
+CREATE DATABASE database_links CHARSET utf8mb4;
+USE database_links;
+
+create table users(
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    email VARCHAR(120) NOT NULL,
+    fullname VARCHAR(100) NOT NULL
+);
+
+create table links(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id INT UNSIGNED,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
