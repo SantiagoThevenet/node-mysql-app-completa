1d19467c1d5b122cc8bcc083d82746a7b86f956e
santiago <santithevenetvalles@gmail.com>
2023-02-15 09:29:09 +0100

    Form created

--- routes/links.js
+++ routes/links.js
@@ -7,8 +7,15 @@ const router = express.Router();
 router.get('/', async(req, res, next) => {
   const [result] = await pool.query('SELECT 1+1')
   res.json(result)
-
-
 });
+
+router.get("/add", (req, res) => {
+  res.render('links/add')
+})
+
+
+router.post("/add", (req, res) => {
+  res.send("Recived")
+})
 
 module.exports = router;
